f097c81c40aea0274638f6f03bacdd2b4bdca57d
Foad Botan <112809457+foadbotan@users.noreply.github.com>
2023-08-26 18:08:12 +1000

    Removed comments

--- api/routes/messageRouter.js
+++ api/routes/messageRouter.js
@@ -6,10 +6,6 @@ const {
 
 const router = express.Router();
 
-/**
- * Query the database for all the registers users
- * @returns {Object} allUsers: A json object with the data
- */
 router.post('/', addMessage);
 router.get('/:roomId', getMessages);
 
