e8344e1f49a0571c0815122121e97e632975c174
Foad Botan <foadbotan@gmail.com>
2023-08-29 21:01:12 +1000

    Add user emails and session to DB

--- api/database/migrations/20230829105745_add_email_and_session/migration.sql
+++ api/database/migrations/20230829105745_add_email_and_session/migration.sql
@@ -8,5 +8,18 @@
 -- AlterTable
 ALTER TABLE "User" ADD COLUMN     "email" VARCHAR(255) NOT NULL;
 
+-- CreateTable
+CREATE TABLE "Session" (
+    "id" TEXT NOT NULL,
+    "sid" TEXT NOT NULL,
+    "data" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Session_sid_key" ON "Session"("sid");
+
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
