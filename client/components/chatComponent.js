5bf9be552309859c309adbe258ff810476513631
Nikita O'Keeffe <snow@Nikitas-MacBook-Air.local>
2023-08-31 20:46:05 +1000

    removed uncessary stuff

--- client/components/chatComponent.js
+++ client/components/chatComponent.js
@@ -92,14 +92,15 @@ function renderChat(room) {
         avatarImg.src = 'https://dl.openseauserdata.com/cache/originImage/files/547152b481352a23d622d9ec71a568e3.png';
         // REPLACE WITH USERS AVATAR
 
-        const deleteButton = document.createElement('button');
-        deleteButton.className = 'delete-button';
-
         messageDiv.appendChild(avatarImg);
 
         const messageP = document.createElement('p');
         messageP.className = 'message';
         messageP.textContent = `user: ${message}`;
+
+        const deleteButton = document.createElement('span');
+        deleteButton.className = 'delete-button';
+        deleteButton.textContent = 'X';
 
         messageP.appendChild(deleteButton);
         messageDiv.appendChild(messageP);
