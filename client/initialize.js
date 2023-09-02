7dadffa141da1e5ca3047b72d5137df063f2853d
Foad Botan <foadbotan@gmail.com>
2023-09-02 13:09:26 +1000

    Refactor frontend

--- client/initialize.js
+++ client/initialize.js
@@ -1,32 +1,10 @@
 import renderChat from './components/chatComponent.js';
 import renderPlaylist from './components/playlistComponent.js';
-import renderRoomsSidebar from "./components/roomSideComponent.js";
+import renderRoomsSidebar from './components/roomSideComponent.js';
 import renderUserStatus from './components/userStatus/renderUserStatus.js';
 
-function renderPage(room) {
-  const pageContainer = document.getElementById('page-container');
-  pageContainer.innerHTML = '';
+renderChat();
+renderPlaylist();
+renderRoomsSidebar();
 
-  const componentsWrapper = document.createElement('div');
-  componentsWrapper.className = 'components-wrapper';
-
-  const navbarContainer = document.createElement('div');
-  navbarContainer.className = "navbar-component";
-  renderRoomsSidebar();
-  componentsWrapper.appendChild(navbarContainer);
-
-  const chatContainer = document.createElement('div');
-  chatContainer.className = 'chat-component';
-  renderChat(room, chatContainer);
-  componentsWrapper.appendChild(chatContainer);
-
-  const playlistContainer = document.createElement('div');
-  playlistContainer.className = 'playlist-component';
-  renderPlaylist(room, playlistContainer);
-  componentsWrapper.appendChild(playlistContainer);
-
-  pageContainer.appendChild(componentsWrapper);
-}
-
-renderPage(1);
 renderUserStatus();
