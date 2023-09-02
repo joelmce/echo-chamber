956c34a94b8b7772126dac537fef951ffa97ddc9
Joel McElroy <joelanthony.mac@gmail.com>
2023-09-02 16:26:12 +1000

    Dynamic rooms

--- client/components/roomSideComponent.js
+++ client/components/roomSideComponent.js
@@ -1,6 +1,19 @@
+import rooms from './roomList.js';
 const newE = (tag) => document.createElement(tag);
 
 function renderRoomsSidebar() {
+  const roomsList = document.getElementById('room-list');
+
+  rooms().then((response) => {
+    response.forEach((room) => {
+      const button = document.createElement('li');
+      button.textContent = room.roomName;
+      button.id = room.roomId;
+      button.className = 'room-name';
+      roomsList.append(button);
+      console.log(button);
+    });
+  });
 }
 
 export default renderRoomsSidebar;
