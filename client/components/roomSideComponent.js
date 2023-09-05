51192aa234dda1806e5d912aeac55e36fca84532
Foad Botan <foadbotan@gmail.com>
2023-09-05 22:13:21 +1000

    Style active room and add title
    Co-authored-by: Alex Slater <al-x-s@users.noreply.github.com>
    Co-authored-by: nikitaokeeffe <nikitaokeeffe@users.noreply.github.com>

--- client/components/roomSideComponent.js
+++ client/components/roomSideComponent.js
@@ -9,14 +9,18 @@ function renderRoomsSidebar() {
     response.forEach((room) => {
       const button = document.createElement('li');
       button.textContent = room.roomName;
-      // button.dataset.id = room.roomId;
       button.className = 'room-name';
       roomsList.append(button);
 
-      button.addEventListener(
-        'click',
-        () => new Room(room.roomId, room.roomName)
-      );
+      button.addEventListener('click', () => {
+        const activeRoom = document.querySelector('.active-room');
+        activeRoom?.classList.remove('active-room');
+        button.classList.add('active-room');
+
+        const chatTitle = document.getElementById('chat-title');
+        chatTitle.textContent = room.roomName;
+        new Room(room.roomId, room.roomName);
+      });
     });
   });
 }
