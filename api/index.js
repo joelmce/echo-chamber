93dbd7ba9513f42b28522b0c5f8703db6a8def50
Foad Botan <foadbotan@gmail.com>
2023-09-07 18:20:01 +1000

    fix localhost bugs

--- api/index.js
+++ api/index.js
@@ -15,8 +15,9 @@ const { PrismaClient } = require('@prisma/client');
  */
 const app = express();
 const port = process.env.PORT || 3000;
+const domain = process.env.DOMAIN || 'http://localhost';
 const server = app.listen(port, () => {
-  console.log(`Server running on ${process.env.DOMAIN}:${port}`);
+  console.log(`Server running on ${domain}:${port}`);
 });
 
 const sessionMiddleware = expressSession({
