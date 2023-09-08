8a53774b01e0d5955f13a26ec309e775911fb506
Foad Botan <foadbotan@gmail.com>
2023-09-08 21:01:42 +1000

    Fix video id bug

--- client/components/Songs/handlePlay.js
+++ client/components/Songs/handlePlay.js
@@ -1,7 +1,6 @@
 function handlePlay(e) {
-  const songId = e.target.dataset.songId;
+  const songId = e.target.parentNode.dataset.songId;
   const iframe = document.querySelector('iframe');
-  iframe.style.display = 'block';
   iframe.src = `https://www.youtube.com/embed/${songId}?autoplay=1`;
 }
 
