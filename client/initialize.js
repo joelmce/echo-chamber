1e8f8bb98cc9a4e1de11fe56b8a4630b467461de
Foad Botan <foadbotan@gmail.com>
2023-09-08 21:13:45 +1000

    fix initialize

--- client/initialize.js
+++ client/initialize.js
@@ -2,14 +2,6 @@ import { handleMessagesSocket } from './components/Messages/handleMessagesSocket
 import { renderNavbar } from './components/Navbar/render.js';
 import { handleSongsSocket } from './components/Songs/handleSongsSocket.js';
 
-<<<<<<< HEAD
 renderNavbar();
-=======
-renderRoomsSidebar();
-
-renderUserStatus();
-
-handleSongsForm();
->>>>>>> main
 handleSongsSocket();
 handleMessagesSocket();
